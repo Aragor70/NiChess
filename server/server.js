180a641c3cfc0f281e7f622f49bc87fc05a787b7
Aragor70 <mikis3@o2.pl>
2021-03-02 21:44:43 +0000

    Accept client http requests

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 connect()
 
 var corsOptions = {
-    origin: ['http://localhost:3000', 'https://nichess.netlify.app/'],
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE']
 };
