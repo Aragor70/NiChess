6de6875e29684f7027d8fb6252a65cc13ad272c2
Aragor70 <mikis3@o2.pl>
2021-03-02 21:31:21 +0000

    Client axios requests config

--- client/src/utils/setAuthToken.ts
+++ client/src/utils/setAuthToken.ts
@@ -3,8 +3,10 @@ import axios from 'axios';
 const setAuthToken = (token: string) => {
 
     if (token) {
+        axios.defaults.withCredentials = true
         return axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
     } else {
+        axios.defaults.withCredentials = true
         return axios.defaults.headers.common['Authorization']
     }
 }
