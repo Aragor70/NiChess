b68fcd6e8512c879b8a1bdda71061ad173bc82bc
Aragor70 <mikis3@o2.pl>
2021-03-03 19:10:27 +0000

    Restore client https io address

--- client/src/interface/Table.tsx
+++ client/src/interface/Table.tsx
@@ -15,7 +15,7 @@ const Table = ({ match, table, getTable, history, initBoard, deleteTable, leaveF
     useEffect(() => {
         getTable(match.params.id)
 
-        socket = io("http://nichess.herokuapp.com")
+        socket = io("https://nichess.herokuapp.com")
 
         connection = setInterval(() => console.log('I am running'), 10000)
 
